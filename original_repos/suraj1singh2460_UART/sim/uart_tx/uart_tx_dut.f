09d55a672ef5d5b222a33310847a20b1d0e65294
Alma Babbitt <asbabbit@asu.edu>
2024-09-04 10:40:26 -0700

    Deleted unecessary original repos

--- original_repos/suraj1singh2460_UART/sim/uart_tx/uart_tx_dut.f
+++
@@ -1,7 +0,0 @@
-E:/UVM/UART/rtl/uart_tx/mux.v
-E:/UVM/UART/rtl/uart_tx/parity_calc.v
-E:/UVM/UART/rtl/uart_tx/serializer.v
-E:/UVM/UART/rtl/uart_tx/tx_fsm.v
-E:/UVM/UART/rtl/uart_tx/uart_tx.v
-
-+incdir+E:/UVM/UART/rtl/uart_tx/
--- original_repos/suraj1singh2460_UART/sim/uart_tx/work/uart_tx_top/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity uart_tx_top is
-end uart_tx_top;
