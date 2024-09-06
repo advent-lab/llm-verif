003726ece1a665d96664da7d320db34ed3972dbd
slowe8 <slowe8@asu.edu>
2024-09-06 13:11:50 -0700

    First try at many generations

--- llm/generate_sha_testbenches.py
+++ llm/generate_sha_testbenches.py
@@ -1,4 +1,5 @@
 import llama3_chat as chat
+from storage import FileStore
 import os
 
 if __name__=="__main__":
@@ -8,17 +9,23 @@ if __name__=="__main__":
     prompt = prompt_file.read()
 
     # Create a directory to store generations
-    if not os.path.isdir('./generations'):
-        os.mkdir('./generations')
+    store = FileStore('./generations')
 
-    # Run generations
-    # Initialize conversation history with system message
-    conversation = [
-        {"role": "system", "content": "You are a verification engineering assistant tasked with generating test benches that meet a coverage requirement of 100% statement coverage."}
-    ]
-    conversation.append({"role":"user", "content":prompt})
+    for i in range(0,100):
+        # Run generations
+        # Initialize conversation history with system message
+        conversation = [
+            {"role": "system", "content": "You are a verification engineering assistant tasked with generating test benches that meet a coverage requirement of 100% statement coverage."}
+        ]
+        conversation.append({"role":"user", "content":prompt})
 
-    chat.load_model()
-    response = chat.generate_response(conversation_history=conversation)
-    print(response)
-    print(chat.convert_json_reponse_to_dict(response))
+        chat.load_model()
+        response = chat.generate_response(conversation_history=conversation)
+        print(response)
+        response = chat.convert_json_response_to_dict(response)
+
+        cov = chat.get_coverage(response, './sha12/design')
+        if cov != "":
+            print("Passed!")
+        else:
+            print("Failed!")
