5c7d4d87ec3ea5166fcfda467eb26e3907f25492
slowe8 <slowe8@asu.edu>
2024-10-08 14:06:45 -0700

    Made some chnages

--- llm/method1_job.sh
+++ llm/method1_job.sh
@@ -31,6 +31,6 @@ pip install -r requirements.txt
 module load bittware/questa-23.4
 export LM_LICENSE_FILE=27006@en4228283l.cidse.dhcp.asu.edu
 
-python evaluate_methodology1.py -d "/home/slowe8/Research/llm_verif_dataset/data_points/chacha_top" -g 10 -c /packages/apps/fpga/Questa/questa_fe/bin #> method1_job.log
+python evaluate_methodology1.py -d /home/slowe8/Research/llm_verif_dataset/data_points/chacha_top -g 10 -c /packages/apps/fpga/Questa/questa_fe/bin #> method1_job.log
 deactivate
 rm -rf venv
