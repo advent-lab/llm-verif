0503bdb9aceae55b53c83e2c5920084675f21064
Sean Lowe <slowe8@asu.edu>
2024-10-08 13:01:36 -0700

    Refactored dashboard.py

--- llm/dashboard.py
+++ llm/dashboard.py
@@ -9,8 +9,11 @@ class Dataset:
         with open(dashboard_path, 'r') as f:
             dashboard_content = f.read()
 
+        self.base_dir = str(Path(dashboard_path).parents[0]) + '/data_points'
+
         decoder = json.JSONDecoder(strict=False)
         self.dataset = decoder.raw_decode(dashboard_content)[0]
+        self.replace_base_dir()
 
     def get_data_point(self, design_name: str) -> Union[dict, None]:
         if design_name in self.dataset.keys():
@@ -19,28 +22,13 @@ class Dataset:
             return None
 
     def get_design_spec(self, design_name: str) -> Union[str, list, None]:
-        if len(self.dataset[design_name]['spec']) == 0:
-            return None
-        elif len(self.dataset[design_name]['spec']) == 1:
-            return self.dataset[design_name]['spec'][0]
-        else:
-            return self.dataset[design_name]['spec']
+        return self.check_list(self.dataset[design_name], 'spec')
 
     def get_design(self, design_name: str) -> Union[str, list, None]:
-        if len(self.dataset[design_name]['design']) == 0:
-            return None
-        elif len(self.dataset[design_name]['design']) == 1:
-            return self.dataset[design_name]['design'][0]
-        else:
-            return self.dataset[design_name]['design']
+        return self.check_list(self.dataset[design_name], 'design')
 
     def get_design_context(self, design_name: str) -> Union[str, list, None]:
-        if len(self.dataset[design_name]['design_context']) == 0:
-            return None
-        elif len(self.dataset[design_name]['design_context']) == 1:
-            return self.dataset[design_name]['design_context'][0]
-        else:
-            return self.dataset[design_name]['design_context']
+        return self.check_list(self.dataset[design_name], 'design_context')
 
     def get_design_and_context(self, design_name: str) -> Union[str, list, None]:
         design = self.get_design(design_name)
@@ -54,6 +42,25 @@ class Dataset:
             return design
         else:
             return design + design_context
+
+    def replace_base_dir(self):
+        for tkey in self.dataset.keys():
+            for dkey in self.dataset[tkey].keys():
+                if isinstance(self.dataset[tkey][dkey], str):
+                    self.dataset[tkey][dkey] = self.dataset[tkey][dkey].replace('$(BASE_DIR)', self.base_dir)
+                elif isinstance(self.dataset[tkey][dkey], list):
+                    for i in range(len(self.dataset[tkey][dkey])):
+                        self.dataset[tkey][dkey][i] = self.dataset[tkey][dkey][i].replace('$(BASE_DIR)', self.base_dir)
+
+    def check_list(self, dlist: Union[str, list], key: str) -> list:
+        if len(dlist[key]) == 0:
+            return None
+        elif len(dlist[key]) == 1:
+            return dlist[key][0]
+        else:
+            return dlist[key]
+
+        
 
 
 
