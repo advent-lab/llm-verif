97d6e0e451b472015d83d218ac9cc559392f491e
slowe8 <slowe8@asu.edu>
2024-10-28 15:07:45 -0700

    Small change

--- llm/method1_job.sh
+++ llm/method1_job.sh
@@ -3,16 +3,17 @@
 #SBATCH -N 1            # number of nodes
 #SBATCH -c 32            # number of cores 
 #SBATCH --mem=128G
-#SBATCH -t 1-00:00:00
-#SBATCH -G a100:3
-#SBATCH -C a100_80
-#SBATCH -p general      # partition 
+#SBATCH -t 0-01:00:00
+#SBATCH -G h100:2
+#SBATCH -p htc      # partition 
 #SBATCH -q public       # QOS
 #SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
 #SBATCH -e slurm.%j.err # file to save job's STDERR (%j = JobId)
 #SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
 #SBATCH --mail-user="%u@asu.edu"
 #SBATCH --export=LM_LICENSE_FILE=27006@en4228283l.cidse.dhcp.asu.edu   # Purge the job-submitting shell environment
+
+echo $LM_LICENSE_FILE
 
 REPO_DIR=$1
 DATA_POINT=$2
@@ -28,6 +29,7 @@ source /home/slowe8/llm_venv/bin/activate
 
 module load bittware/questa-23.4
 export LM_LICENSE_FILE=27006@en4228283l.cidse.dhcp.asu.edu
+echo $LM_LICENSE_FILE
 
 python evaluate_methodology1.py -d $REPO_DIR/data_points/$DATA_POINT -g 10 -c /packages/apps/fpga/Questa/questa_fe/bin #> method1_job.log
 deactivate
