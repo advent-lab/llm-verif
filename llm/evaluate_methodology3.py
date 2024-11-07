bef280f2c0cc4e4b70f92a12be281b65a243a1a8
slowe8 <slowe8@asu.edu>
2024-11-07 14:11:11 -0700

    Fixed an error

--- llm/evaluate_methodology3.py
+++ llm/evaluate_methodology3.py
@@ -26,6 +26,8 @@ if __name__=="__main__":
 
     for i in range(0,runs):
 
+        record.max_cov = 0
+
         # Run generations
         # Initialize conversation history with system message
         print(f"\n\nRun {i}")
--- llm/evaluate_methodology6.py
+++ llm/evaluate_methodology6.py
@@ -26,6 +26,9 @@ if __name__=="__main__":
     runs = args.generations
 
     for i in range(0,runs):
+
+        record.max_cov = 0
+
         # Run generations
         # Initialize conversation history with system message
         print(f"\n\nRun {i}")
