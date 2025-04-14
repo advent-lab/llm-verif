c65ea56ff84ea61ede7ef91835c9e0aa5cb1509f
slowe8 <slowe8@asu.edu>
2025-04-14 13:22:04 -0700

    Changes to my scripts

--- llm/scripts/job_logarithmic_temperature.sh
+++ llm/scripts/job_logarithmic_temperature.sh
@@ -3,10 +3,10 @@
 #SBATCH -N 1            # number of nodes
 #SBATCH -c 32            # number of cores 
 #SBATCH --mem 64GB
-#SBATCH -t 0-10:00:00
+#SBATCH -t 0-06:00:00
 #SBATCH -G a100:2
 #SBATCH -C a100_80
-#SBATCH -p htc      # partition 
+#SBATCH -p general      # partition 
 #SBATCH -q public       # QOS
 #SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
 #SBATCH -e slurm.%j.err # file to save job's STDERR (%j = JobId)
@@ -21,17 +21,17 @@ RUN=$4
 
 #Load required software
 #Change to the directory of our script
-rm -rf $REPO_DIR/llm/venv /scratch/$USER/temperature_runs/${DATA_POINT}_logarithmic
-mkdir /scratch/$USER/temperature_runs/
-mkdir /scratch/$USER/temperature_runs/${DATA_POINT}_logarithmic_$RUN
+rm -rf $REPO_DIR/llm/venv /scratch/$USER/runs/${DATA_POINT}_logarithmic
+mkdir /scratch/$USER/runs/
+mkdir /scratch/$USER/runs/${DATA_POINT}_logarithmic_$RUN
 cp -rf $REPO_DIR/llm/evaluations \
     $REPO_DIR/llm/src \
     $REPO_DIR/llm/build_llm_venv.sh \
     $REPO_DIR/llm/requirements.txt \
     $REPO_DIR/llm/accelerate_config.yaml \
-    /scratch/$USER/temperature_runs
+    /scratch/$USER/runs
 
-cd /scratch/$USER/temperature_runs
+cd /scratch/$USER/runs
 
 #Run the software/python script
 ./build_llm_venv.sh
@@ -40,9 +40,9 @@ source /home/$USER/llm_venv/bin/activate
 module load bittware/questa-23.4
 export LM_LICENSE_FILE=27006@en4228283l.scai.dhcp.asu.edu
 
-cd /scratch/$USER/temperature_runs
+cd /scratch/$USER/runs
 
-python /scratch/$USER/temperature_runs/evaluations/evaluate_methodology6.py \
+python /scratch/$USER/runs/evaluations/evaluate_methodology6.py \
     -d $REPO_DIR/data_points/$DATA_POINT \
     -g $NUM_RUNS \
     -c /packages/apps/fpga/Questa/questa_fe/bin \
@@ -54,7 +54,7 @@ python /scratch/$USER/temperature_runs/evaluations/evaluate_methodology6.py \
     --temperature_function "logarithmic" \
     --max_iterations 20 \
     --max_valid_iter 10 \
-    -o /scratch/$USER/temperature_runs/${DATA_POINT}_logarithmic_$RUN \
-    2>&1 | tee /scratch/$USER/temperature_runs/${DATA_POINT}_logarithmic_$RUN/${DATA_POINT}_logarithmic_$RUN.log
+    -o /scratch/$USER/runs/${DATA_POINT}_logarithmic_$RUN \
+    2>&1 | tee /scratch/$USER/runs/${DATA_POINT}_logarithmic_$RUN/${DATA_POINT}_logarithmic_$RUN.log
 
 deactivate
--- llm/scripts/job_mixed_design_runs.sh
+++ llm/scripts/job_mixed_design_runs.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-DESIGNS=("sha1_top" "cryptech_uart" "trng_top" "chacha_top")
+DESIGNS=("sha1_top" "cryptech_uart" "chacha_top")
+ITER=(0 1 2)
 
 #"vndecorrelator" "cryptech_uart" "fifo" "trng_top" 
 
 for DESIGN in "${DESIGNS[@]}"; do
-	sbatch job_constant_testplan.sh ~/Research/llm_verif_dataset $DESIGN 3 123
-	sbatch job_constant_batch.sh ~/Research/llm_verif_dataset $DESIGN 3 123
-	sbatch job_constant_temperature.sh ~/Research/llm_verif_dataset $DESIGN 3 123
-	sbatch job_logarithmic_temperature.sh ~/Research/llm_verif_dataset $DESIGN 3 123
-	sbatch job_sigmoid_temperature.sh ~/Research/llm_verif_dataset $DESIGN 3 123
+	for RUN in ${ITER[@]}; do
+		sbatch job_constant_testplan.sh ~/Research/llm_verif_dataset $DESIGN 1 $RUN
+		sbatch job_constant_batch.sh ~/Research/llm_verif_dataset $DESIGN 1 $RUN
+		sbatch job_constant_temperature.sh ~/Research/llm_verif_dataset $DESIGN 1 $RUN
+		sbatch job_logarithmic_temperature.sh ~/Research/llm_verif_dataset $DESIGN 1 $RUN
+		sbatch job_sigmoid_temperature.sh ~/Research/llm_verif_dataset $DESIGN 1 $RUN
+	done
 done
