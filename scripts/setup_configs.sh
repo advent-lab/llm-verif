90a5feacd3815c6ff1c65383c331416857b9c823
Sean Lowe <slowe8@asu.edu>
2025-08-28 11:16:51 -0700

    Added some helper scripts I have been using

---
+++ scripts/setup_configs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+TEMPERATURE_FUNCTIONS=("constant" "logarithmic" "capped_sigmoid")
+REMOVE_POLLUTED_CONTEXT=(1 0)
+BATCH_SIZES=(1 5)
+TESTPLAN=(1 0)
+
+
+mkdir -p configs
+
+for func in "${TEMPERATURE_FUNCTIONS[@]}"; do
+  for remove_polluted_context in "${REMOVE_POLLUTED_CONTEXT[@]}"; do
+    for batch in "${BATCH_SIZES[@]}"; do
+      for testplan in "${TESTPLAN[@]}"; do
+
+        FILE="configs/base_env_${func}_${remove_polluted_context}_${batch}_${testplan}.env"
+        echo "Generating $FILE"
+
+        cat > "$FILE" <<EOF
+OPENAI_API_KEY=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
+COMPILER=/mnt/vault0/tools/Intel/intelFPGA_pro/23.4/questa_fe/bin
+MERGE_COVERAGE=1
+SIM_RUNS=20
+MAX_ITERATIONS=20
+MAX_VALID_ITER=10
+TEMPERATURE_FUNCTION=${func}
+BATCH_SIZE=${batch}
+TESTPLAN=${testplan}
+REMOVE_POLLUTED_CONTEXT=${remove_polluted_context}
+RUNS=2
+EOF
+      done
+    done
+  done
+done
