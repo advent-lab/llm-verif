78305b754bd7257e35c2b54a800eff969c11790d
slowe8 <slowe8@asu.edu>
2025-10-03 21:05:48 -0700

    Making llama stable branch

--- llm_verif/llm_verif.py
+++ llm_verif/llm_verif.py
@@ -10,7 +10,7 @@ from llm_verif import __version__ as VERSION
 from llm_verif.environment import Environment
 from llm_verif.questasim import QuestaSim
 from llm_verif.simulator import CoverageResponse
-from llm_verif.chatgpt_chat import ChatGPTChat
+from llm_verif.llama3_chat import LlamaChat
 import argparse
 from llm_verif.record import Record
 from llm_verif.util import run_conversation
@@ -150,7 +150,7 @@ def main():
         include_merge_coverage=args.merge_coverage
     )
 
-    llm = ChatGPTChat(
+    llm = LlamaChat(
         QuestaSim(args.compiler, environment.design_module_name), 
         environment, 
         do_sample=not args.no_sampling,
